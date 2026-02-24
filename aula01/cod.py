fdc736276ea53e9484e175b722ead7beeb5f450b
Ana Carolina Sá <133174965+accoelhos@users.noreply.github.com>
2026-02-24 18:31:33 +0000

    add aula 01

---
+++ aula01/cod.py
@@ -0,0 +1,29 @@
+# codigo da aula 24/02
+
+# importacoes
+import cv2 #visao computacional
+import numpy # trabalhar com matrizes
+
+# leitura da imagem com a funcao imread()
+imagem = cv2.imread('gato-preto-deitado.jpg')
+# a img eh armazenada na variavel imagem
+# a imagem eh uma magriz de 3 dimensoes -> 3 canais RGB (red, green, blue)
+# cada celula da matriz eh um pixel com valor de 0 a 255
+# cada celula contem um inteiro de 8 bits sem sinal (uint8)
+# cada pixel eh formado por uma tupla de 3 inteiros de 8 bits
+
+# largura da imagem 
+print('largura em pixels: ', imagem.shape[1])
+
+# altura da imagem
+print('altura em pixels: ', imagem.shape[0])
+
+# qtd de canais da imagem (colorida possui 3 canais)
+print('qtd de canais: ', imagem.shape[2])
+
+# mostra img na tela com imshow
+cv2.imshow('Nome da janela', imagem)
+cv2.waitKey(0) # espera o usuario apertar uma tecla para fechar a janela
+
+# salvar a img no disco com a funcao imwrite()
+cv2.imwrite('imagem_copia.jpg', imagem) # salva a img com o nome 'imagem_copia.jpg'
---
+++ aula01/notas.md
@@ -0,0 +1,13 @@
+## Aula 24/02
+
+- Livro recomendado: processamento digital de imagens 3 edição
+
+- Imagem -> conj. de matrizes (pixel)
+- **Amostragem** : número de ponotos amosrados de uma img digitalizada (resolução)
+- **Quantização** : qtd de níveis de tons que pode ser atribuído a cada ponto digitalizado
+- Faixa de variação de tom: 0 até 255 (preto até branco absoluto)
+- Resolução tem a ver com a amostragem. Custa armazenamento.
+- Um pixel é caracterizado pelo valor de tonalidade de cor e pela sua localização na imagem  
+- Resol. Espacial -> Quanto maior o número de pixels, melhor é a qualidade da imagem
+- 
+
